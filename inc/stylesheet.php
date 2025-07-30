e096b51a3c91b5f10030f15db5cb814e4bde167a
weiyee <edgetechnology456@gmail.com>
2025-07-30 18:03:32 +0800

    add meta tag & sitemap

--- inc/stylesheet.php
+++ inc/stylesheet.php
@@ -1,4 +1,5 @@
 <meta name="robots" content="noindex, nofollow">
+<meta name="google-site-verification" content="yKaolSwxO0xvK-MjAV9NU8FGtDw_WD8GuiMgg4jAdSg" />
 <link rel="icon" type="image/png" href="<?php echo $site_base_url.'images/favicon.png';?>" />
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
