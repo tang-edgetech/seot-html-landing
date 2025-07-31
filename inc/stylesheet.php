7b5c4122a88fb38fb887155c06e55b767b485b47
tang.edgetech <tang.edgetech@gmail.com>
2025-07-31 17:05:24 +0800

    Upload sitemap.xml and meta google verification

--- inc/stylesheet.php
+++ inc/stylesheet.php
@@ -1,4 +1,5 @@
 <meta name="robots" content="noindex, nofollow">
+<meta name="google-site-verification" content="NRkW0looYJm_S0embn13pXYADBmd2qPUyoyJaHswcKg" />
 <link rel="icon" type="image/png" href="<?php echo $site_base_url.'/images/favicon.png';?>" />
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
