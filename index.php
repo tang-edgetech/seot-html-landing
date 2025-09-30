8bbb24676f4d697ae03e3e8db168337a1dbaa4e6
tsheng <tsheng@itcom.it.com>
2025-09-30 17:56:17 +0800

    Type in banner link

--- index.php
+++ index.php
@@ -217,7 +217,7 @@ include 'inc/functions.php';
                 <div class="container-fluid">
                     <div class="row justify-content-center">
                         <div class="col-12 px-3">
-                            <img class="w-100" src="<?= $site_base_url;?>/images/img-banner-clubmy-first-deposit-.webp" alt="promo banner">
+                            <img class="w-100" src="<?= $site_base_url;?>/images/img-banner-clubmy-first-deposit.webp" alt="promo banner">
                         </div>
                     </div>
                 </div>
