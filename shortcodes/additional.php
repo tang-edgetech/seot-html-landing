ada819c92efc33c392b5470167fff0b15d3a56e3
Tang Edgetech <tang.edgetech@gmail.com>
2026-03-13 12:14:27 +0800

    Adding in Author Card

--- shortcodes/additional.php
+++ shortcodes/additional.php
@@ -1,3 +1,22 @@
 <?php
-echo 'Additional Section';
+
 ?>
+<section class="section-author-card">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 px-4">
+                <div class="author-card">
+                    <div class="author-card-inner d-flex flex-column flex-md-row justify-content-start align-items-center align-items-md-start text-center text-md-start gap-4">
+                        <div class="author-thumbnail"><img src="<?= assets_url();?>author-rahimidin-razif.webp"/></div>
+                        <div class="author-body flex">
+                            <h3 class="author-name mb-0">Rahimidin Razif</h3>
+                            <span class="d-block meta author-designation mb-3">Penulis Kandungan Digital</span>
+                            <div class="author-description mb-4"><p>Rahimidin Razif ialah penulis kandungan digital yang memfokuskan kepada panduan pengguna, analisis platform permainan dalam talian dan struktur operasi laman web hiburan digital.</p></div>
+                            <a href="<?= home_url();?>/author/" class="btn btn-default py-2 px-4"><span>Visit Profile</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
